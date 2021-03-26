2f2c4b739d9709d504046fb12eda33ac932a88f9
Arimoroolamilekan <arimoroolamilekan1@gmail.com>
2021-03-26 09:00:06 +0100

    Initial Commit

--- upload.js
+++ upload.js
@@ -13,25 +13,43 @@ const submitResume = () => {
   };
 
   if (resume) {
-    fetch(
-      "https://3uxec4442l.execute-api.us-east-1.amazonaws.com/default/getPresignedURL",
-      {
-        method: "PUT",
-        body: JSON.stringify(
-          `{             
-              "uploadURL": "${postResume.uploadURL}"
-              "photoFilename" : "${postResume.photoFilename}",
-          }`
-        ),
-        headers: {
-          "Content-Type": "text/plain",
+    axios
+      .put(
+        "https://3uxec4442l.execute-api.us-east-1.amazonaws.com/default/getPresignedURL",
+        {
+          postResume: postResume,
+        }
+      )
+      .then(
+        (response) => {
+          console.log(response);
         },
-      }
-    ).then(function (postResume) {
-      alert("Congrats");
-    });
-  } else {
-    swal("Hey!", "Please fill in your details", "warning");
+        (error) => {
+          console.log(error);
+        }
+      );
   }
+
+  //   if (resume) {
+  //     fetch(
+  //       "https://3uxec4442l.execute-api.us-east-1.amazonaws.com/default/getPresignedURL",
+  //       {
+  //         method: "POST",
+  //         body: JSON.stringify(
+  //           `{
+  //               "uploadURL": "${postResume.uploadURL}"
+  //               "photoFilename" : "${postResume.photoFilename}",
+  //           }`
+  //         ),
+  //         headers: {
+  //           "Content-Type": "text/plain",
+  //         },
+  //       }
+  //     ).then(function (postResume) {
+  //       alert("Congrats");
+  //     });
+  //   } else {
+  //     swal("Hey!", "Please fill in your details", "warning");
+  //   }
   //   console.log(`${resume}`);
 };
